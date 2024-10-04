27bb932ba03013edcccafe4f6370f94e90e5dde5
Paul Horn <dev@knutwalker.engineer>
2024-10-04 17:51:55 +0200

    codecrafters submit [skip ci]

--- src/main.zig
+++ src/main.zig
@@ -4,8 +4,10 @@ const net = std.net;
 pub fn main() !void {
     const stdout = std.io.getStdOut().writer();
 
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    try stdout.print("Logs from your program will appear here!\n", .{});
+    const ally = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer ally.deinit();
+
+    // const alloc = ally.allocator();
 
     // Uncomment this block to pass the first stage
     const address = try net.Address.resolveIp("127.0.0.1", 4221);
@@ -14,6 +16,8 @@ pub fn main() !void {
     });
     defer listener.deinit();
 
-    _ = try listener.accept();
-    try stdout.print("client connected!", .{});
+    const connection = try listener.accept();
+    try stdout.print("client connected from {}!", .{connection.address});
+
+    try connection.stream.writeAll("HTTP/1.1 200 OK\r\n\r\n");
 }
